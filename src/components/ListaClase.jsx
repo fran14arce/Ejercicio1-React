6cfa3388e603428a6fe7d1550a00ee34e0f84382
fran14arce <farcer01@educantabria.es>
2022-01-11 12:37:33 +0100

    Revisión, salta error de módulos

--- src/components/ListaClase.jsx
+++ src/components/ListaClase.jsx
@@ -1,33 +1,66 @@
 import React from 'react';
+import ComponenteListaClase from './ComponenteListaClase';
 
-import Lista from './Lista';
-import ListaClase from './ListaClase';
+class ListaClase extends React.Component {
+  constructor(props) {
+    super(props);
+    this.titulo = props.titulo;
+    this.icono = props.icono;
+    this.state = {
+      listaComponentes: this.initList(props.elementos),
+    };
+    this.valorTextInput = React.createRef();
+    this.valorPrioritySelect = React.createRef();
+  }
 
-export function PaginaListas() {
-  const elementos1 = [
-    { done: false, texto: 'Hacer la compra', prioridad: 'alta' },
-    { done: true, texto: 'Sacar al perro', prioridad: 'media' },
-  ];
-  const elementos2 = [
-    { done: false, texto: 'Tomates' },
-    { done: true, texto: 'Queso' },
-    { done: false, texto: 'Leche' },
-  ];
-  const elementos3 = [{ done: false, texto: 'Estudiar React.js' }];
+  initList(elementos) {
+    const listaInicial = [];
+    for (let i = 0; i < elementos.length; i++) {
+      listaInicial.push(
+        <ComponenteListaClase
+          done={elementos[i].done}
+          texto={elementos[i].texto}
+          prioridad={elementos[i].prioridad}
+        />
+      );
+    }
+    return listaInicial;
+  }
 
-  return (
-    <div>
-      <h1>Listas de tareas</h1>
-      <Lista
-        titulo="Tareas de la casa (funciones)"
-        icono="♥"
-        elementos={elementos1}
+  addElement() {
+    const newLista = this.state.listaComponentes.concat(
+      <ComponenteListaClase
+        texto={this.valorTextInput.current.value}
+        prioridad={this.valorPrioritySelect.current.value}
       />
-      <ListaClase
-        titulo="ToDos del curso (clases)"
-        icono="🙌"
-        elementos={elementos3}
-      />
-    </div>
-  );
+    );
+    this.setState({ listaComponentes: newLista });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.titulo} - {this.icono}
+        <ul>
+          {this.state.listaComponentes}
+          <li>
+            <input
+              ref={this.valorTextInput}
+              type="text"
+              placeholder="Introduce una tarea"
+            />
+            <br />
+            <select ref={this.valorPrioritySelect}>
+              <option value="alta">Prioridad Alta</option>
+              <option value="media">Prioridad Media</option>
+              <option value="baja">Prioridad Baja</option>
+            </select>
+            <button onClick={this.addElement.bind(this)}>Añadir</button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default ListaClase;
