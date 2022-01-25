e3cc16ff8c36097527d2b4d35cdc88d448101fa7
fran14arce <farcer01@educantabria.es>
2022-01-25 11:50:46 +0100

    Se añade un formulario en home para hacer una especie de login sencillo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
-export function Home() {
-  return (
-    <div>
-      <h1>¡Bienvenido!</h1>
-    </div>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: '', password: '' };
+    this.login = this.login.bind(this);
+  }
+  login() {
+    this.setState({ user: this.valorUsuario.value, password: this.valorContraseña.value });
+    console.log(`${this.state.user} Enviado`);
+  }
+  render() {
+    if (
+      this.state != null &&
+      this.state.user != null &&
+      this.state.user != ''
+    ) {
+      return (
+        <div className="main-site">
+          <h1>Bienvenido {this.state.user}!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="main-site">
+          <h1>Bienvenido!</h1>
+          <Container>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Nombre usuario</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Usuario"
+                  ref = {(usuario) => (this.valorUsuario = usuario)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Contraseña"
+                  ref = {(contraseña) => (this.valorContraseña = contraseña)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Recordarme" />
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={this.login}>
+                Entrar
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      );
+    }
+  }
 }
+export default Home;
